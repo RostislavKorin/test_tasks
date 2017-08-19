af7f2d95829ad333b031458876ebf8e9d08f0f3a
Rostislav Korin <rostislav.korin@gmail.com>
2017-08-19 19:51:52 +0300

    fix

--- spec/move_elements_spec.rb
+++ spec/move_elements_spec.rb
@@ -4,9 +4,9 @@ require 'move_elements'
 describe 'move' do
 	it 'move_elements' do
 		expect(MoveElements.move_elements([1, 2, 5, 1, 5])).to eq([5, 2, 1, 5, 1])
-    expect(MoveElements.move_elements([0, 12, 3, 2])).to eq([12, 0, 3, 2])
-    expect(MoveElements.move_elements([0])).to eq([0])
+		expect(MoveElements.move_elements([0, 12, 3, 2])).to eq([12, 0, 3, 2])
+		expect(MoveElements.move_elements([0])).to eq([0])
 		expect(MoveElements.move_elements([0, 1, 3, 2])).to eq([3, 1, 0, 2])
-    expect(MoveElements.move_elements([12, 0])).to eq([0, 12])
+		expect(MoveElements.move_elements([12, 0])).to eq([0, 12])
 	end
 end
